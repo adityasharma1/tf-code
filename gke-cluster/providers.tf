c9e60319a6a42ef439f4a8c88b7620c818613e86
adityasharma1 <aditya.mk.sharma@gmail.com>
2021-03-27 01:20:16 +0000

    gke TF code first draft

---
+++ gke-cluster/providers.tf
@@ -0,0 +1,20 @@
+# setup the GCP provider | provider.tf
+
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.53"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.gcp_region_1
+  zone    = var.gcp_zone_1
+}
+
+data "google_client_config" "default" {}
+
---
+++ gke-cluster/terraform.tfvars
@@ -0,0 +1,8 @@
+# GCP Settings
+project_id   = "deloitte-demo-308622"
+gcp_region_1 = "us-central1"
+gcp_zone_1   = "us-central1-a"
+
+# Application Name (used in resource names, no spaces.)
+app_name = "demo-events-feed"
+
